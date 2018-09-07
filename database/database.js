b08b4aac0c4ef55ced52266779865e837eabe4c3
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-09-07 14:50:57 -0700

    Set up database connection and schemas

---
+++ database/database.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+/* --------------------------------------------------------------------------------------------- */
+
+// Set up Mongoose Connection
+mongoose.connect('mongodb://localhost/test');
+const db = mongoose.connection;
+db.on('error', () => console.error('connection error:'));
+/* --------------------------------------------------------------------------------------------- */
+
+// Set up Schemas
+const ratingSchema = new mongoose.Schema({
+  rating: Number,
+  review: String,
+  last_updated: { type: Date, default: Date.now },
+  helpfulness: Number,
+  user: [String]
+});
+
+const dealSchema = new mongoose.Schema({
+  name: String,
+  type: String,
+  ratings: [ratingSchema]
+});
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  reviews: Number,
+  ratings: Number
+});
+/* --------------------------------------------------------------------------------------------- */
+
+// Set up Models
+const Deal = mongoose.model('Deal', dealSchema);
+const User = mongoose.model('User', userSchema);
+/* --------------------------------------------------------------------------------------------- */

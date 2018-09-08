4707f3270f11869aa3384bd03bb509b630e9f17e
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-09-07 17:36:36 -0700

    Add all routes functionality

--- server/server.js
+++ server/server.js
@@ -1,23 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('../database/database');
 
 // Set up Express server and JSON parsing of API requests
 const app = express();
 app.use(bodyParser.json());
-app.listen('http://localhost:3002', () => console.log('Listening on 3001...'));
+app.listen('http://localhost:3002', () => console.log('Listening on 3002...'));
 /* --------------------------------------------------------------------------------------------- */
 
 // Set up routes
 app.get('/:dealId/api/ratings', (req, res) => {
-  if (req.query.total !== undefined && req.query.average !== undefined) {
-  } else if (req.query.total !== undefined) {
-  } else if (req.query.average !== undefined) {
-  } else {
-    res.sendStatus(400);
-  }
-  res.send();
+  db.getRatings(req.params.dealId)
+    .then(result => {
+      if (req.query.total !== undefined && req.query.average !== undefined) {
+        res.send(result);
+      } else if (req.query.total !== undefined) {
+        res.send({ total: result.total });
+      } else if (req.query.average !== undefined) {
+        res.send({ average: result.average });
+      } else {
+        res.sendStatus(400);
+      }
+    })
+    .catch(err => {
+      res.sendStatus(500);
+      throw err;
+    });
 });
 
 app.get('/:dealId/api/reviews', (req, res) => {
-  res.send();
+  db.getAllReviews(req.params.dealId)
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      res.sendStatus(500);
+      throw err;
+    });
 });

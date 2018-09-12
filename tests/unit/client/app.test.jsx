dc954cf18ae130e924f3e98131dfef33fc905521
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-09-11 21:55:53 -0700

    Add basic outer container and unit test

---
+++ tests/unit/client/app.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from '../../../client/src/app';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Test App Component', () => {
+  const app = shallow(<App />);
+
+  test('should render component', () => {
+    expect(app.exists()).toBeTruthy();
+  });
+
+  test('should render container heading', () => {
+    expect(app.find('h1').text()).toEqual('Customer Reviews');
+    expect(app.find('h1').length).toEqual(1);
+  });
+});

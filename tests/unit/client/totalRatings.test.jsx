42cccc2135b665c5fb1b0e9e344eb34d5cbf6a46
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-09-12 15:39:19 -0700

    Add test for totalRatings

---
+++ tests/unit/client/totalRatings.test.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import TotalRatings from '../../../client/src/totalRatings';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Test App Component', () => {
+  const ratings = shallow(<TotalRatings average={3.3} total={27} />);
+
+  test('should render component', () => {
+    expect(ratings.exists()).toBeTruthy();
+  });
+
+  test('should render stars', () => {
+    expect(ratings.find('FontAwesomeIcon').length).toEqual(5);
+    expect(ratings.find('FontAwesomeIcon').filter('[color="gold"]').length).toEqual(3);
+  });
+  test('should render total ratings count', () => {
+    expect(
+      ratings
+        .find('div')
+        .first()
+        .text()
+        .indexOf('(27 Ratings)')
+    ).toBeGreaterThan(-1);
+  });
+});

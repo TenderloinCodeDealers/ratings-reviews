30172e5af97f4875efb523b2c559772df5716ec8
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-09-13 19:56:53 -0700

    Get total and average ratings from server

--- client/src/totalRatings.jsx
+++ client/src/totalRatings.jsx
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 
-const TotalRatings = ({ total, average }) => {
+const TotalRatings = ({ totalRatings, avgRatings }) => {
   const stars = [];
   for (let index = 1; index <= 5; index += 1) {
-    const color = index <= average ? '#FDC038' : '#A5A8AB';
+    const color = index <= avgRatings ? '#FDC038' : '#A5A8AB';
     stars[index] = (
       <FontAwesomeIcon className="star" key={`star-${index}`} icon={faStar} color={color} />
     );
@@ -15,14 +15,14 @@ const TotalRatings = ({ total, average }) => {
     <div id="total-ratings-container">
       {stars}
       &emsp;
-      <span id="total-ratings-count">({total} Ratings)</span>
+      <span id="total-ratings-count">({totalRatings} Ratings)</span>
     </div>
   );
 };
 
 TotalRatings.propTypes = {
-  total: PropTypes.number.isRequired,
-  average: PropTypes.number.isRequired
+  totalRatings: PropTypes.number.isRequired,
+  avgRatings: PropTypes.number.isRequired
 };
 
 export default TotalRatings;

981c6235540d00aa1e97fdf5fc02a6a24bc1e2f2
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-09-13 18:10:59 -0700

    Add styling for total and average ratings component

--- client/src/totalRatings.jsx
+++ client/src/totalRatings.jsx
@@ -6,14 +6,16 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
 const TotalRatings = ({ total, average }) => {
   const stars = [];
   for (let index = 1; index <= 5; index += 1) {
-    const color = index <= average ? 'gold' : 'grey';
-    stars[index] = <FontAwesomeIcon key={`star-${index}`} icon={faStar} color={color} />;
+    const color = index <= average ? '#FDC038' : '#A5A8AB';
+    stars[index] = (
+      <FontAwesomeIcon className="star" key={`star-${index}`} icon={faStar} color={color} />
+    );
   }
   return (
-    <div color="blue">
+    <div id="total-ratings-container">
       {stars}
-      &emsp;(
-      {total} Ratings)
+      &emsp;
+      <span id="total-ratings-count">({total} Ratings)</span>
     </div>
   );
 };

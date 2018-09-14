9f1b0ca7e416a0d62c768779be51a73c2ae68a3f
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-09-13 17:02:10 -0700

    Added styling to verified guarantee component

--- client/src/verifiedGuarantee.jsx
+++ client/src/verifiedGuarantee.jsx
@@ -3,10 +3,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBadgeCheck } from '@fortawesome/pro-solid-svg-icons';
 
 const VerifiedGuarantee = () => (
-  <div>
-    <FontAwesomeIcon icon={faBadgeCheck} color="#56A227" />
-    <strong>100% Verified Reviews</strong>
-    <p>All reviews are from people who have redeemed deals with this merchant.</p>
+  <div id="guarantee-container">
+    <FontAwesomeIcon id="guarantee-check" icon={faBadgeCheck} />
+    <div>
+      <strong>100% Verified Reviews</strong>
+      <p id="guarantee-description">
+        All reviews are from people who have redeemed deals with this merchant.
+      </p>
+    </div>
   </div>
 );
 

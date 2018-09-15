419efeb12219baf971134a3130caeda426432146
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-09-15 14:08:17 -0700

    Create Review Component

---
+++ client/src/review.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TimeAgo from 'react-timeago';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+const Review = ({ reviewObject }) => {
+  const { user, rating, review, last_updated, helpfulness } = reviewObject;
+  const stars = [];
+  for (let index = 1; index <= 5; index += 1) {
+    const color = index <= rating ? '#FDC038' : '#A5A8AB';
+    stars[index] = (
+      <FontAwesomeIcon className="star" key={`star-${index}`} icon={faStar} color={color} />
+    );
+  }
+
+  return (
+    <div>
+      <h2>User Component goes here</h2>
+      <div>
+        {stars}
+        &thinsp; · &thinsp;
+        <TimeAgo date={last_updated} />
+      </div>
+      <p>{review}</p>
+      <button type="button">Helpfulness {helpfulness}</button>
+    </div>
+  );
+};
+
+Review.propTypes = {
+  reviewObject: PropTypes.shape({
+    user: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      reviews: PropTypes.number.isRequired,
+      rating: PropTypes.number.isRequired
+    }).isRequired,
+    rating: PropTypes.number.isRequired,
+    review: PropTypes.number.isRequired,
+    last_updated: PropTypes.number.isRequired,
+    helpfulness: PropTypes.number.isRequired
+  }).isRequired
+};
+
+export default Review;

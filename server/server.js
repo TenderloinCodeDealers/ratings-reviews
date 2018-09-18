db3f8d2e7d66ab601f0a66eb326b9b97441b1c87
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-09-17 23:36:12 -0700

    Add CORS Header to responses

--- server/server.js
+++ server/server.js
@@ -4,9 +4,26 @@ const db = require('../database/database');
 
 // Set up Express server and JSON parsing of API requests
 const app = express();
+// Allow Cross Origin Requests
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Expose-Headers', 'Content-Length');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Accept, Authorization, Content-Type, X-Requested-With, Range'
+  );
+  if (req.method === 'OPTIONS') {
+    return res.send(200);
+  }
+  return next();
+});
 app.use('/:id', express.static('client/dist'));
 app.use(bodyParser.json());
 app.listen(3002, () => console.log('Listening on 3002...'));
+/* --------------------------------------------------------------------------------------------- */
+
 /* --------------------------------------------------------------------------------------------- */
 
 // Set up routes

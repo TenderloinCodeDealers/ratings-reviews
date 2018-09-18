6537d085543519a81a42ce8fa47e293de017739d
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-09-17 23:33:45 -0700

    Switch to using styled components

--- client/src/totalRatings.jsx
+++ client/src/totalRatings.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+/* Styling */
+const Container = styled.div`
+  margin-bottom: 10px;
+`;
+
+const Count = styled.span`
+  line-height: 18px;
+  font-size: 18px;
+  color: #0093ea;
+`;
+
+const Star = styled.div`
+  font-size: 17px;
+  display: inline-block;
+`;
 
 const TotalRatings = ({ totalRatings, avgRatings }) => {
   const stars = [];
   for (let index = 1; index <= 5; index += 1) {
     const color = index <= avgRatings ? '#FDC038' : '#A5A8AB';
     stars[index] = (
-      <FontAwesomeIcon className="star" key={`star-${index}`} icon={faStar} color={color} />
+      <Star key={`star-${index}`}>
+        <FontAwesomeIcon icon={faStar} color={color} />
+      </Star>
     );
   }
   return (
-    <div id="total-ratings-container">
+    <Container>
       {stars}
       &emsp;
-      <span id="total-ratings-count">({totalRatings} Ratings)</span>
-    </div>
+      <Count>({totalRatings} Ratings)</Count>
+    </Container>
   );
 };
 
--- client/src/verifiedGuarantee.jsx
+++ client/src/verifiedGuarantee.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBadgeCheck } from '@fortawesome/pro-solid-svg-icons';
 
+/* Styling */
+const Container = styled.div`
+  background-color: #f6f7f8;
+  padding: 10px 0 10px 0;
+  display: flex;
+`;
+
+const Check = styled.div`
+  padding: 0 10px 0 10px;
+  font-size: 40px;
+  color: #56a227;
+`;
+
+const Description = styled.div`
+  margin: 0;
+  padding-top: 5px;
+`;
+
 const VerifiedGuarantee = () => (
-  <div id="guarantee-container">
-    <FontAwesomeIcon id="guarantee-check" icon={faBadgeCheck} />
+  <Container>
+    <Check>
+      <FontAwesomeIcon id="guarantee-check" icon={faBadgeCheck} />
+    </Check>
     <div>
       <strong>100% Verified Reviews</strong>
-      <p id="guarantee-description">
+      <Description>
         All reviews are from people who have redeemed deals with this merchant.
-      </p>
+      </Description>
     </div>
-  </div>
+  </Container>
 );
 
 export default VerifiedGuarantee;
